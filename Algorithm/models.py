6d4de55869573aec0128fd9d783e107a6e05d552
aniket965 <aniket965.as@gmail.com>
2019-05-18 22:52:57 +0530

    feat: Added few models

---
+++ Algorithm/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+
+class Language(models.Model):
+    lang_name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.lang_name
+
+    def __unicode__(self):
+        return self.lang_name
+
+
+class Algorithm(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    langs = models.ManyToManyField(Language)
+    created_at = models.DateTimeField('created_at', auto_now=True)
+    updated_at = models.DateTimeField('updated_at', auto_now=True)
+    category = models.ForeignKey(
+        Category, models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+
+class Code(models.Model):
+	algo = models.ForeignKey(Algorithm, on_delete=models.CASCADE)
+	content = models.TextField(max_length=20000, null=False)
+	lang = models.ForeignKey(Language, on_delete=models.CASCADE)
+
