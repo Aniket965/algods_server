d06bda74522b86b5afe4346f9330d9f26d61b977
aniket965 <aniket965.as@gmail.com>
2020-02-11 09:49:49 +0530

    feat: env variable

--- algods_server/settings.py
+++ algods_server/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,8 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+# load env variables from .env file
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*l*5r2$go1-9@r=%gzwtkpyl83$!kch)99kon2)rg^-)-cn^bl'
+SECRET_KEY = os.environ['ALGO_DS_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
